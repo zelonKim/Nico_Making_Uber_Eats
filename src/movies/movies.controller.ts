43219bddf264de488ebd026fd27112309b76e2b6
zelonKim <ksz18601@gmail.com>
2023-08-30 21:11:45 +0900

    Controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+}
+
+/* 
+  @Get() // GET http://localhost:3000/movies
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search') // GET localhost:3000/movies/search?year=1995
+  search(@Query('year') searchingYear: string) {
+    // 쿼리스트링 키의 해당 값을 가져온 후, 이를 searchingYear 변수에 담아줌.
+    return `We are searching for a movie released after ${searchingYear}`; // We are searching for a movie released after 1995
+  }
+
+  @Post() // POST http://localhost:3000/movies
+  create(@Body() movieData) {
+    return movieData; // Body에 입력된 값이 출력됨. { "name": "Seongjin", "director": "Nico" }
+  }
+
+  @Delete('/:id') // DELETE http://localhost:3000/movies/1
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id ${movieId}`; // This will delete a movie with the id 1
+  }
+
+  @Patch('/:id') // PATCH http://localhost:3000/movies/1
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    }; // { "updatedMovie": "1", "name": "Seongjin", "director": "Nico" }
+  } 
+  */
